8ba9b49c0e7f76bdc7e0b86b8b391c57a108cf3a
jujin <jujin1324@daum.net>
2019-08-01 13:08:23 +0900

    first commit2

---
+++ hello-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MyName from './MyName';
+import Counter from './Counter';
+
+function App() {
+    return (
+        <div className="App">
+            {/* <MyName name="유주진" /> */}
+            {/* <MyName /> */}
+            <Counter />
+        </div>
+        
+    );
+}
+
+export default App;
---
+++ phone-book/src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
+
+class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id : 0,
+        name : '도민준',
+        phone: '010-1234-1234'
+      },
+      {
+        id : 1,
+        name: '홍길동',
+        phone: '010-3214-3213'
+      }
+    ],
+    keyword: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information : information.concat({ id: this.id++, ...data })
+    })
+  }
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information : information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id, data) => {
+    const {information} = this.state;
+    this.setState({
+      information : information.map(
+        info => id === info.id ? { ...info, ...data} : info
+      )
+    })
+  }
+
+  render() {
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
+    return (
+      <div>
+        <PhoneForm 
+          onCreate={this.handleCreate}
+          />
+          <p>
+            <input placeholder="검색할 이름을 입력하세욧..." 
+            onChange={this.handleChange}
+            value={keyword}/>
+          </p>
+          <hr />
+        <PhoneInfoList data={filteredList} 
+                  onRemove={this.handleRemove}
+                  onUpdate={this.handleUpdate}
+        />
+      </div>
+    );
+
+  }
+}
+
+export default App;

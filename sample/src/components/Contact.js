71aca2e324aa448806d2a8c13c5b2b95fe921807
jujin <jujin1324@daum.net>
2019-08-08 22:26:10 +0900

    Little Changed

--- sample/src/components/Contact.js
+++ sample/src/components/Contact.js
@@ -30,6 +30,24 @@ class Contact extends Component {
         this.handleEdit = this.handleEdit.bind(this);
     }
 
+    componentWillMount() {
+        console.log("componentWillMount");
+        const contactData = localStorage.contactData;
+
+        if (contactData) {
+            this.setState({
+                contactData : JSON.parse(contactData)
+            })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate");
+        if(JSON.stringify(prevState.contactData) !== JSON.stringify(this.state.contactData)) {
+            localStorage.contactData = JSON.stringify(this.state.contactData);
+        }
+    }
+
     handleChange(e) {
         this.setState({
             keyword: e.target.value
